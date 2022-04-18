3308cb1c7f82c841f2e8c94b196b76d21eea10a8
junghee <okela2002@naver.com>
2022-04-18 20:24:11 +0900

    no message

--- src/main/java/CovidProcess/CovidController.java
+++ src/main/java/CovidProcess/CovidController.java
@@ -12,7 +12,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import vo.*;
+import vo.BoardVO;
 
 /**
  * Servlet implementation class potController
